de72a8076a8f4401c10e4071c66e6a96cc25d802
aadesh <aadeshchauhan27@gmail.com>
2023-01-20 23:26:07 +0530

    initial commit of mern dashboard

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import bodyParser from "body-parser";
+import express from "express";
+import helmet from "helmet";
+import mongoose from "mongoose";
+import nodemon from "nodemon";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use();
